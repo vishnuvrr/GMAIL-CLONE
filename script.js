1952d86f8b3ebce601e3a3f1f83544fb1191b32b
R Vishnu Vardhan Reddy <83265815+vishnuvrr@users.noreply.github.com>
2021-07-16 17:22:41 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -43,29 +43,112 @@ function handleAuthResult(authResult) {
 
 function loadGmailApi() {
   gapi.client.load('gmail', 'v1', displayInbox);
+  // console.log(gapi.client.getToken());
 }
 
-
 function displayInbox() {
+  document.getElementById("content").innerHTML="";
   var request = gapi.client.gmail.users.messages.list({
     'userId': 'me',
     'labelIds': 'INBOX',
     'maxResults': 20
   });
-
   request.execute(function(response) {
     $.each(response.messages, function() {
       var messageRequest = gapi.client.gmail.users.messages.get({
         'userId': 'me',
         'id': this.id
       });
-
       messageRequest.execute(appendMessageRow);
     });
   });
 }
 
+function loadSentApi()
+{
+  gapi.client.load('gmail', 'v1', displaySent);
+  console.log(gapi.client.gmail.users.messages);
+}
+
+function displaySent()
+{
+  document.getElementById("content").innerHTML="";
+  var request = gapi.client.gmail.users.messages.list({
+    'userId': 'me',
+    'labelIds': 'SENT',
+    'maxResults': 20
+  });
+  request.execute(function(response) {
+    $.each(response.messages, function() {
+      var messageRequest = gapi.client.gmail.users.messages.get({
+        'userId': 'me',
+        'id': this.id
+      });
+      messageRequest.execute(appendSentMessageRow);
+    });
+  });
+}
+
+function loadDraftApi()
+{
+  gapi.client.load('gmail', 'v1', displayDraft);
+  console.log(gapi.client.gmail.users.messages);
+}
+
+function displayDraft()
+{
+  document.getElementById("content").innerHTML="";
+  var request = gapi.client.gmail.users.messages.list({
+    'userId': 'me',
+    'labelIds': 'DRAFT',
+    'maxResults': 20
+  });
+  
+  request.execute(function(response) {
+    $.each(response.messages, function() {
+      var messageRequest = gapi.client.gmail.users.messages.get({
+        'userId': 'me',
+        'id': this.id
+      });
+      messageRequest.execute(appendSentMessageRow);
+    });
+  });
+}
+
+function appendSentMessageRow(message)
+{
+  try
+{
+  var data=getHeader(message.payload.headers, 'To');
+  var subject=getHeader(message.payload.headers,'Subject');
+  var accordianItem=document.createElement("div");
+  accordianItem.classList.add("accordion-item");
+
+  accordianItem.innerHTML=`<h2 class="accordion-header" id=${message.id+"id"}>
+  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#id${message.id}" aria-expanded="false" aria-controls=id${message.id}>
+     ${data} 
+  </button>
+</h2>
+<div id=id${message.id} class="accordion-collapse collapse" aria-labelledby=${message.id+"id"} data-bs-parent="#content">
+  <div class="accordion-body">
+    Subject : ${subject}
+    <hr><hr>
+    ${message.snippet}
+  </div>
+</div>`
+ document.getElementById("content").appendChild(accordianItem);
+}
+catch(err)
+{
+  console.log(err);
+}
+
+
+}
+
+
 function appendMessageRow(message) {
+ 
 try
 {
   var data=getHeader(message.payload.headers, 'From');
